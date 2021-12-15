9cdfc6ca0c974f036214f09ef7bc03ecc954819c
Jeffrey Martinez <martinezjf2@gmail.com>
2021-12-15 13:22:42 -0500

    added notes on installing pytest and installd pytest

--- first_commit.py
+++ first_commit.py
@@ -1,3 +1,7 @@
 
 hello_world = 'Hello World'
 print(hello_world)
+
+# pip package management system to do specific tasks for packages
+# install pytest by doing this command: pip install -U pytest
+
