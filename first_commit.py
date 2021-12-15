b97813bd0107d7d18bebc436d5c904088527bdd1
Jeffrey Martinez <martinezjf2@gmail.com>
2021-12-15 13:31:04 -0500

    first test in python

--- first_commit.py
+++ first_commit.py
@@ -5,3 +5,12 @@ print(hello_world)
 # pip package management system to do specific tasks for packages
 # install pytest by doing this command: pip install -U pytest
 
+def func(x):
+    return x + 1
+
+def test_answer():
+    assert func(4) == 5
+    
+    
+# to run python file run this code: python <directory path>
+# to run pytest run this code: pytest <directory path>
